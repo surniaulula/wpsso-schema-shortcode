969777220afefa60811dd7c5f56f414333410c90
JS Morisset <jsm@surniaulula.com>
2021-09-20 13:19:17 -0700

    v1.0.0-dev.7

--- wpsso-schema-shortcode.php
+++ wpsso-schema-shortcode.php
@@ -10,7 +10,7 @@
  * Author URI: https://wpsso.com/
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
- * Description: Schema shortcode for advanced users.
+ * Description: Shortcode for advanced Schema markup of content sections.
  * Requires PHP: 7.0
  * Requires At Least: 5.0
  * Tested Up To: 5.8.1
