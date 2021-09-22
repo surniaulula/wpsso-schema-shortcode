250b4d007ed885d743d74a6773759d140af0a136
JS Morisset <jsm@surniaulula.com>
2021-09-22 07:52:53 -0700

    v1.0.0-b.2

--- wpsso-schema-shortcode.php
+++ wpsso-schema-shortcode.php
@@ -14,8 +14,8 @@
  * Requires PHP: 7.0
  * Requires At Least: 5.0
  * Tested Up To: 5.8.1
- * WC Tested Up To: 5.6.0
- * Version: 1.0.0-b.1
+ * WC Tested Up To: 5.7.0
+ * Version: 1.0.0-b.2
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
