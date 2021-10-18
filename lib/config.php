febbb884b0d180ccc51a79cb62480f520fc9bf39
JS Morisset <jsm@surniaulula.com>
2021-10-18 13:50:24 -0700

    v1.1.0-rc.1

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoSscConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssossc' => array(			// Plugin acronym.
-					'version'     => '1.1.0-dev.1',	// Plugin version.
+					'version'     => '1.1.0-rc.1',	// Plugin version.
 					'opt_version' => '1',		// Increment when changing default option values.
 					'short'       => 'WPSSO SSC',	// Short plugin name.
 					'name'        => 'WPSSO Schema Shortcode',
