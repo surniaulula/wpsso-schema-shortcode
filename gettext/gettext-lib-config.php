c193d165d89042cba59e1d4be597c593f303c3c8
JS Morisset <jsm@surniaulula.com>
2021-11-13 20:34:08 -0800

    v1.2.0-dev.3

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -5,3 +5,4 @@
  */
 _x( 'WPSSO Schema Shortcode', 'plugin name', 'wpsso-schema-shortcode' );
 _x( 'Schema shortcode to define and customize additional properties and types for sections of the content (WPSSO Core Premium Required).', 'plugin description', 'wpsso-schema-shortcode' );
+_x( 'WPSSO Core (Premium)', 'plugin name', 'wpsso-schema-shortcode' );
