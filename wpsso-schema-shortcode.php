80283da6eaf8aac489516841b51096d43f2270ad
JS Morisset <jsm@surniaulula.com>
2022-10-11 06:53:13 -0700

    v1.3.2

--- wpsso-schema-shortcode.php
+++ wpsso-schema-shortcode.php
@@ -13,7 +13,7 @@
  * Description: Schema shortcode to define and customize additional properties and types for sections of the content (WPSSO Core Premium Required).
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 6.0.2
+ * Tested Up To: 6.1.0
  * WC Tested Up To: 6.9.4
  * Version: 1.3.2
  * 
