85b46f906129969471e12d71a4afdce9459409bd
JS Morisset <jsm@surniaulula.com>
2023-01-24 10:14:29 -0800

    v1.4.1-dev.3

--- wpsso-schema-shortcode.php
+++ wpsso-schema-shortcode.php
@@ -13,7 +13,7 @@
  * Description: Schema shortcode to define and customize additional properties and types for sections of the content.
  * Requires Plugins: wpsso
  * Requires PHP: 7.2
- * Requires At Least: 5.2
+ * Requires At Least: 5.4
  * Tested Up To: 6.1.1
  * Version: 1.4.1-dev.3
  *
