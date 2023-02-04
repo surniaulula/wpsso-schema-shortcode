9c372095a1427bd8bb607852b23e7858e1175b43
JS Morisset <jsm@surniaulula.com>
2023-02-04 08:10:47 -0800

    v1.4.1

--- wpsso-schema-shortcode.php
+++ wpsso-schema-shortcode.php
@@ -67,6 +67,9 @@ if ( ! class_exists( 'WpssoSsc' ) ) {
 			load_plugin_textdomain( 'wpsso-schema-shortcode', false, 'wpsso-schema-shortcode/languages/' );
 		}
 
+		/*
+		 * Called by Wpsso->set_objects which runs at init priority 10.
+		 */
 		public function init_objects() {
 
 			$this->p =& Wpsso::get_instance();
